42426cebbd540993e8081524d02e9526a68ffc1a
高橋啓太 <keita@takahashikeitanoMacBook-Pro.local>
2017-09-14 17:07:05 +0900

    gofmt

--- main.go
+++ main.go
@@ -1,20 +1,19 @@
 package main
 
 import (
+	"github.com/go-spotify/handler"
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
-	"Go-Spotify/handler"
 )
 
-
-func main(){
+func main() {
 	e := echo.New()
 	e.Pre(middleware.HTTPSRedirect())
 	e.Use(middleware.Logger())
-    e.Use(middleware.Recover())
+	e.Use(middleware.Recover())
 
-	e.GET("/",handler.OAuthHandler)
-	e.GET("/callback/",handler.CallbackHandler)
+	e.GET("/", handler.OAuthHandler)
+	e.GET("/callback/", handler.CallbackHandler)
 
 	//HTTPSを使うのに必要！
 	e.Logger.Fatal(e.StartTLS(":3000", "cert.pem", "key.pem"))
