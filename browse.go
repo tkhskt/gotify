8250f86964bce6f396f18769dd20a973a7537bd4
Keita <bonjovi1926@yahoo.co.jp>
2017-12-16 21:45:57 +0900

    update README

---
+++ browse.go
@@ -0,0 +1,29 @@
+package gotify
+
+import (
+	"encoding/json"
+
+	"github.com/gericass/gotify/extensions"
+	"github.com/gericass/gotify/models"
+)
+
+func (t *Tokens) GetBrowseFeaturedPlaylists() (*models.BrowseFetaturedPlaylists, error) {
+	/**
+	https://developer.spotify.com/web-api/get-list-featured-playlists/
+	*/
+
+	endpoint := "https://api.spotify.com/v1/browse/featured-playlists"
+
+	res, err := extensions.Request(endpoint, t.AccessToken)
+	if err != nil {
+		return nil, err
+	}
+
+	browseFeaturedPlaylists := new(models.BrowseFetaturedPlaylists)
+
+	err = json.Unmarshal(res, browseFeaturedPlaylists)
+	if err != nil {
+		return nil, err
+	}
+	return browseFeaturedPlaylists, nil
+}
